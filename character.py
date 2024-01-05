4f6b67dba07dd0a5c3aa56b448bdf3abe5526d45
Brendan Raines <brendan.raines@gmail.com>
2024-01-06 10:09:17 +1100

    add inventory parameter and setters

--- character.py
+++ character.py
@@ -2,11 +2,12 @@
 
 class character():
     
-    def __init__(self, name: str, id: int = 0) -> None:
+    def __init__(self, name: str, id: int = 0, inv_size: int = 5) -> None:
         self.id = id
         self.name = name
         self.exp = 0
         self.health = 100
+        self.inv_size = inv_size
         
     def get_name(self) -> str:
         return self.name
@@ -17,10 +18,17 @@ class character():
     def get_health(self) -> int:
         return self.health
     
+    def get_inv_size(self) -> int:
+        return self.inv_size
+    
     def add_exp(self, exp_mod: int) -> None:
         self.exp += exp_mod
 
     def change_health(self, health_mod: int) -> None:
         self.health += health_mod
 
-    def set_name
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def change_inv_size(self, inv_change: int = 0) -> None:
+        self.inv_size += inv_change
